88562aaabba3f743ff5e5d2704a03aa14cacd356
Luka <luka.toni93@gmail.com>
2014-06-21 18:31:58 +0200

    Create HW-9.c

---
+++ HW-9.c
@@ -0,0 +1,315 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 1000
+#define MIL 1000000
+
+int length = 0;
+
+//-------MEMORY STRING---------//
+char* makeMemory()
+{
+   char *tabel = malloc(sizeof(char)*MIL);
+   tabel[0] = '\0';
+   return tabel;
+}
+
+char** makeTable()
+{
+   char** newTab = malloc(sizeof(char*)*MAX);
+   //int i;
+   return newTab;
+}
+
+void fileReadStandard(char *data1)
+{
+    char line[MAX];
+    int i = 0;
+    int j = 0;
+
+    while (gets(line) !=  NULL)
+    {
+       i=0;
+      while (line[i] != '\0')
+      {
+          if((line[i] == ' ') || (line[i] == '\t') || (line[i] == '\n'))
+          {
+              i++;
+              continue;
+          }
+          else
+          {
+              data1 = line[i];
+              i++;
+              j++;
+          }
+      }
+   }
+}
+
+//---------READ DATA----------//
+void fileRead(char *pathData, char *dataRewrite)
+{
+   FILE *file = fopen (pathData, "r");
+   /*ce ni mogoce odpreti datoteke*/
+   if(file == NULL)
+   {
+       printf("napaka 1\n");
+       exit(0);
+   }
+
+   char line[MAX];
+   int i = 0;
+   int j = 0;
+
+   while (fgets(line, MAX, file) !=  NULL)
+   {
+       i=0;
+      while (line[i] != '\0')
+      {
+          if((line[i] == ' ') || (line[i] == '\t') || (line[i] == '\n'))
+          {
+              i++;
+              continue;
+          }
+          else
+          {
+              dataRewrite[j] = line[i];
+              i++;
+              j++;
+          }
+      }
+   }
+   fclose(file);
+}
+
+//-------------CREAT MEMORY FOR TAG---------------//
+char *memoryString(char *tag1, char *tag)
+{
+   int len = strlen(tag);
+   tag1 = malloc(sizeof(char)*(len+4)); //4 za znake v +
+   tag1[0] = '\0';
+   return tag1;
+}
+
+//-------------GET VALUE BETWEEN TAGS-----------------//
+char* getValue(char *tag, char *niz) /*pregled XML-ja */
+{
+   //---NASTAVI ZACETNI IN KONCNI TAG------//
+   char *tag1 = memoryString(tag1,tag);
+  /* tag2 = '<'+'\'+tag+'>'*/
+   strcat(tag1, "<");
+   strcat(tag1, tag);
+   strcat(tag1, ">");
+
+   char *tag2 = memoryString(tag2,tag);
+   /* tag2 = '<'+'\'+tag+'>'*/
+   strcat(tag2, "<");
+   strcat(tag2, "/");
+   strcat(tag2, tag);
+   strcat(tag2, ">");
+
+   //--------IZPISE VSE KAR JE VMES-------------//
+   char *ptr1 = strstr(niz, tag1);
+   //printf("ptr1 =%p    tag1: %s\n", ptr1, tag1);
+   char *ptr2 = strstr(niz, tag2);
+  // printf("ptr2 =%p    tag2: %s\n", ptr2, tag2);
+
+   int lenT1 = strlen(tag1);
+
+   char *between = malloc(sizeof(char)*1000);
+
+   int dolg = (int)ptr2 - (int)ptr1 - lenT1;
+
+
+  if(dolg == 0)        /**Handling za napako 2*/
+  {
+      printf("napaka 2");
+      exit(0);
+  }
+
+   strncpy(between, ptr1 + lenT1, dolg );
+
+   between[dolg] = '\0';
+
+   /*nastavimo zadnji pointer za pregled celotne tabele ocena*/
+   free(tag1);
+   free(tag2);
+   return between;
+}
+
+//-------CHECK XML---------------//
+void check(char *data, char** ID, char** PREDMETI, char** OCENA)
+{
+   int len = strlen(data);
+
+   //----CHECK OCENE------------------//
+   char *pData = data;
+   char *pointer = strstr(data, "<ocene>");
+
+   if(!(pData == pointer))
+   {
+       printf("napaka 2\n");
+        exit(1);
+   }
+
+   char *ERRORpointer = strstr(pointer+7, "<ocene>");
+
+   if(!(ERRORpointer == NULL))
+   {
+       printf("napaka 2\n");
+        exit(1);
+   }
+
+   char *pointerEnd = strstr(data, "</ocene>");
+
+   int umes = pointerEnd-pointer;
+
+   if(!((umes+8)==len))
+   {
+       printf("napaka 3\n");  /**------------Tuki si meu napaka 2 pa je blo outputu narobe. Mislm da mors tud v 127 vrstici tud 2 pa tud gor nism cist
+       zihr c je kul napaka 2. Mislm da je tm napaka 3 k zaceten tag ni enak koncnemu ------------------*/
+        exit(0);
+   }
+  // puts("OCENE OK ");
+   //puts(data);
+   /**while*/
+   char* buffer;
+   char* curr = data;
+   char* nID;
+   char* nPR;
+   char* nOC;
+   int j = 0;
+   int updated = 0;
+   while(1)
+   {
+       if(curr + 15 == pointer+strlen(data))
+           break;
+        updated = 0;
+       buffer = getValue("ocena", curr);
+       curr += strlen(buffer) + 15;
+       //puts(buffer);
+      nID = getValue("vpisnaStevilka", buffer);
+      //ID[i] = nID;
+       nPR = getValue("nalogaId", buffer);
+      // PREDMETI[i] = nPR;
+       nOC = getValue("vrednost", buffer);
+      // OCENA[i] = nOC;
+
+       for(j = 0; j < length; j++)
+       {
+           if(strcmp(nID, ID[j]) == 0)
+               if(strcmp(nPR, PREDMETI[j]) == 0)
+               {
+                   OCENA[j] = nOC;
+                   updated++;
+                   break;
+               }
+
+       }
+
+       if(updated == 0)
+       {
+           ID[length] = nID;
+           PREDMETI[length] = nPR;
+           OCENA[length] = nOC;
+           length++;
+           continue;
+       }
+   }
+
+}
+
+//--------BUUBLE SORT FOR PRINTF-------//
+void BubbleSort(char *ID[],char *PREDMET[], char *OCENA[], int array_size)
+{
+    int i, j;
+    char *tempID= makeMemory(tempID);
+    char *tempPredmet= makeMemory(tempPredmet);
+    char *tempOcena= makeMemory(tempOcena);
+
+    for (i = 0; i < (array_size -1); ++i)
+    {
+         for (j = 0; j < array_size - 1 - i; ++j )
+         {
+              if (atoi(ID[j]) > atoi(ID[j+1]))
+              {
+                   tempID = ID[j+1];
+                   ID[j+1] = ID[j];
+                   ID[j] = tempID;
+
+                   tempPredmet = PREDMET[j+1];
+                   PREDMET[j+1] = PREDMET[j];
+                   PREDMET[j] = tempPredmet;
+
+                   tempOcena = OCENA[j+1];
+                   OCENA[j+1] = OCENA[j];
+                   OCENA[j] = tempOcena;
+              }
+         }
+    }
+}
+
+void sort(char *ID[], char *PREDMETI[], char *OCENA[])
+{
+   int lengOfTabels = length;
+    int i, j;
+
+       BubbleSort(ID, PREDMETI, OCENA, lengOfTabels);
+
+       int ass = 0;
+       int grade = 0;
+   for(i = 0; i < lengOfTabels; i++)
+   {
+       if(ID[i][0] == NULL)
+           continue;
+       ass = 0;
+       grade = 0;
+       for(j = 0; j < lengOfTabels; j++)
+       {
+           if( strcmp(ID[i], ID[j]) == 0  )
+           {
+               ass++;
+               grade += atoi(OCENA[j]);
+               if( i != j)
+               ID[j][0] = NULL;
+
+           }
+       }
+       sprintf(OCENA[i], "%d", grade);
+       sprintf(PREDMETI[i], "%d", ass);
+       printf("%s %s %s\n",ID[i], PREDMETI[i], OCENA[i]);
+   }
+}
+
+
+//------------MAIN--------------//
+int main(int argc, char *argv[])
+{
+   /*naredimo tabelo za prepis iz prve datoteke*/
+   char *data1 = NULL;
+   data1 = makeMemory(data1);
+    if(argc == 1)
+    {
+        fileReadStandard(data1);
+    }
+
+    if(argc == 2)
+    {   /*prepisimo datoteko 1 v tabelo data1*/
+        fileRead(argv[1], data1);
+    }
+
+   char **ID = makeTable();
+   char **PREDMET = makeTable();
+   char **OCENA = makeTable();
+   check(data1, ID, PREDMET, OCENA);
+
+   /*izpis pravilni*/
+   sort(ID, PREDMET, OCENA);
+
+  /*test prepisovanja v tabelo*/
+  //printf("%s\n", data1);
+
+  return 0;
+}
